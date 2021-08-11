597601189835a5f532c3bcebe9f00178329a7045
Natavar Prajapati <prajapatinatavar21197@gmail.com>
2021-08-11 22:20:01 +0530

    day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final int days = 30;
+    final String name = "Indialone";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catelog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Hello $name, Welcome to $days Days of Flutter'),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    // const pi = 3.14;
+    // bool isMale = false;
+    // num temperature = 30;
+    // var age = 23;
+
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text('30 Days of Flutter'),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
