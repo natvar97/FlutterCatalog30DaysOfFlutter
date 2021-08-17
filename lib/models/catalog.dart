2458d504b469e3f8391b71ec0625a8a26893abf9
Natavar Prajapati <prajapatinatavar21197@gmail.com>
2021-08-17 22:08:39 +0530

    day13 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,7 +1,7 @@
 class CatalogModel {
   static final items = [
     Item(
-        id: "1",
+        id: 1,
         name: "iPhone 12 Pro",
         desc: "Apple iPhone 12th generation",
         price: 999,
@@ -12,7 +12,7 @@ class CatalogModel {
 }
 
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 import 'package:flutter_catalog/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final int days = 30;
+
+  final String name = "Indialone";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodedData = jsonDecode(catalogJson);
+    final productsData = decodedData["products"];
+    print(productsData);
+  }
 
   @override
   Widget build(BuildContext context) {
     final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
-    final int days = 30;
-    final String name = "Indialone";
     return Scaffold(
       appBar: AppBar(
         title: Text("Catelog App"),
